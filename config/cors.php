daa34108afff7f67f6ff922fc8c7517fb8878005
Daniel Erni <erni@MKali.Kali>
2024-12-22 17:54:30 -0300

    Configuracion dominio en cors

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,16 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    
 
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => ['*'],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
+    'paths' => ['api/*'], 
+    'allowed_methods' => ['*'], 
+    'allowed_origins' => ['https://appstore-bab3d.web.app'], 
+    'allowed_headers' => ['*'], 
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => false,
+    'supports_credentials' => true, 
+    
+    
 
 ];
